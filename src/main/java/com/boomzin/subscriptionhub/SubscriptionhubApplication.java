eab1c195574b340bb9467abe7522ebb28a440edd
Ernest Aidinov <ernest.aidinov@media-tel>
2025-02-09 12:54:05 +0300

    Initial commit

---
+++ src/main/java/com/boomzin/subscriptionhub/SubscriptionhubApplication.java
@@ -0,0 +1,13 @@
+package com.boomzin.subscriptionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boomzin/subscriptionhub/SubscriptionhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boomzin.subscriptionhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
