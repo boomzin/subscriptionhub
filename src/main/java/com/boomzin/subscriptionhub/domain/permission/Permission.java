02840c9969f3a67f03f3fdb16dc2d240bce6dd09
Ernest Aidinov <ernest.aidinov@media-tel>
2025-02-19 16:00:15 +0300

    minor fix

--- src/main/java/com/boomzin/subscriptionhub/domain/permission/Permission.java
+++ src/main/java/com/boomzin/subscriptionhub/domain/permission/Permission.java
@@ -3,11 +3,12 @@ package com.boomzin.subscriptionhub.domain.permission;
 import com.boomzin.subscriptionhub.common.UuidEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
