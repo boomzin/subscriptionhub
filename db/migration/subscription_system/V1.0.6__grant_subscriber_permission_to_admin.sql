fc607c2d30691c60156e8199aff4d0d1980ecb52
Ernest Aidinov <ernest.aidinov@media-tel>
2025-03-02 10:12:38 +0300

    gramt subscriber permission to admin

---
+++ db/migration/subscription_system/V1.0.6__grant_subscriber_permission_to_admin.sql
@@ -0,0 +1,3 @@
+-- Связывание роли "administration" с разрешением "subscriberAccess"
+INSERT INTO role_permissions (role_id, permission_id)
+VALUES ('00000000-0000-0000-0000-000000000001', '7e2e572c-db03-4a2b-9afb-8f31d009591b');
